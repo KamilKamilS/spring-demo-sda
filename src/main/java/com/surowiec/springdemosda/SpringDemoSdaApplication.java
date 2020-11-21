acb1965195314077d21db3c469849ff04ef7db96
Kamil <s.kamilx@gmail.com>
2020-11-21 13:30:44 +0100

    [init] - project initialization

---
+++ src/main/java/com/surowiec/springdemosda/SpringDemoSdaApplication.java
@@ -0,0 +1,13 @@
+package com.surowiec.springdemosda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoSdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoSdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surowiec/springdemosda/SpringDemoSdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surowiec.springdemosda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoSdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
