be9acba4115c3494684f3c892a68825a58e54191
Kamil <s.kamilx@gmail.com>
2020-11-28 09:46:15 +0100

    add post controler

--- src/main/java/com/surowiec/springdemosda/controller/MyFirstController.java
+++ src/main/java/com/surowiec/springdemosda/controller/MyFirstController.java
@@ -3,6 +3,7 @@ package com.surowiec.springdemosda.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +15,10 @@ public class MyFirstController {
     public String myHomePage() {
         log.info("inside home page");
         return "index";
+    }
+
+    @PostMapping("/post")
+    public String post() {
+        return "post";
     }
 }
